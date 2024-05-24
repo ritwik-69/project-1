af73628a68edc1803255ee2f4900e3d7d7a5a4b2
ritwik-69 <72665321+ritwik-69@users.noreply.github.com>
2024-05-24 16:50:59 +0530

    initilizing the project with little bit of frontend and backend done

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import { CssBaseline,ThemeProvider } from "@mui/material";
+import {createTheme} from "@mui/material/styles"
+import { useMemo } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { themeSettings } from "theme";
+import Layout from "scenes/layout"
+import Dashboard from "scenes/dashboard"
+import { useSelector } from "react-redux";
+
+function App() {
+  //setting up redux
+  const mode = useSelector((state) => state.global.mode);
+  // setting up MUI
+  const theme = useMemo(() => createTheme(themeSettings(mode),[mode]))
+  return (
+    <div className="app">
+      <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          {/* this is so that Layout component has navbar and sidebar always */}
+          <Route element={<Layout/>} >
+            <Route path="/" element={<Navigate to="/dashboard" replace/>} />
+            <Route path="/dashboard" element={<Dashboard/>} />
+
+            </Route>
+        </Routes>
+      </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
